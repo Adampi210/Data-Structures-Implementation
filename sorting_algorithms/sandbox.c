4ea7ca9d26c1adcc063f6479274b4e201a576b97
Adam Piaseczny <adampi210u@gmail.com>
2022-12-01 13:31:01 -0500

    modified testing

--- sorting_algorithms/sandbox.c
+++ sorting_algorithms/sandbox.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
 
     int size_arr = 1e6;
     int lower_bound_arr = 0;
-    int upper_bound_arr = 100000;
+    int upper_bound_arr = 100;
     data_type* test_arr = generate_random_arr(size_arr, lower_bound_arr, upper_bound_arr);
     
     //#define TEST_QUICKSORT
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[]) {
         total_runtime = ((double) (end_time - start_time)) / CLOCKS_PER_SEC * 1000;
         printf("Quicksort ran in: %.2lfms.\n", total_runtime);
     #endif
-    //#define TEST_HEAPSORT
+    #define TEST_HEAPSORT
     #ifdef TEST_HEAPSORT
         start_time = clock();
         heapsort_w_downward_heapify(test_arr, size_arr);
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[]) {
         total_runtime = ((double) (end_time - start_time)) / CLOCKS_PER_SEC * 1000;
         printf("Heapsort ran in: %.2lfms.\n", total_runtime);
     #endif
-    #define TEST_MERGESORT
+    //#define TEST_MERGESORT
     #ifdef TEST_MERGESORT
         start_time = clock();
         mergesort(test_arr, size_arr, mergesort_recursive);
